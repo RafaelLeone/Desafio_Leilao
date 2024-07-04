76935daa001d04847da7361420f9b291654c483c
RafaelLeone <rinaciodacosta@gmail.com>
2024-07-04 14:20:36 -0300

    added nav bar

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ docker-compose up --build
 (check if needs to install npm or vue@cli)
 
 docker exec -it django_1 bash
-python manage.py migrate (check)
-python manage.py loaddata initial_data.json
+python manage.py migrate (check if needed)
+python manage.py loaddata initial_data.json (may need a first registered user)
 
 Django: localhost:8000/api
 Vue: localhost:8080
--- front-leilao/src/App.vue
+++ front-leilao/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <TopNavBar @dateChange="fetchSchedule" @toggleLive="toggleLiveEvents"/>
     <router-view /> <!-- This is where the components corresponding to the routes will be rendered -->
   </div>
 </template>
 
 <script>
+import TopNavBar from './components/TopNavBar.vue';
+
 export default {
+  components: {
+    TopNavBar,
+  },
   // No need to import any components here since they will be rendered dynamically based on the routes
 };
 </script>
